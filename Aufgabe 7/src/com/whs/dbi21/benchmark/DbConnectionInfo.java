e7d5ce3ec6c8f2cb8a4b99d332b3ea70840b6a0e
timothy3001 <t.knufmann@gmail.com>
2015-11-18 14:58:50 +0100

    Init connection implementiert, Anmeldedaten sind in DbConnectionInfo zu ändern

---
+++ Aufgabe 7/src/com/whs/dbi21/benchmark/DbConnectionInfo.java
@@ -0,0 +1,7 @@
+package com.whs.dbi21.benchmark;
+
+public class DbConnectionInfo {
+	public final static String JDBCSTRING = "jdbc:mysql://Datenbank-PC/benchmarktest";
+	public final static String DBUSER = "root";
+	public final static String DBPASSWORD = "daten2";
+}
--- Aufgabe 7/src/com/whs/dbi21/benchmark/Main.java
+++ Aufgabe 7/src/com/whs/dbi21/benchmark/Main.java
@@ -1,9 +1,23 @@
 package com.whs.dbi21.benchmark;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import com.whs.dbi21.benchmark.DbConnectionInfo;
+
 public class Main {
 
+	private static Connection dbCon;
+	
 	private static boolean initializeConnection() {
-		return true;
+		try {
+			dbCon = DriverManager.getConnection(DbConnectionInfo.JDBCSTRING, DbConnectionInfo.DBUSER, DbConnectionInfo.DBPASSWORD);
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -11,5 +25,7 @@ public class Main {
 			System.out.println("Could not establish connection to database!");
 			System.exit(-1);
 		}			
+		
+				
 	}
 }
