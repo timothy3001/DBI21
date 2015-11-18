b4062431eac56f934956fc0c49739ff3f218c68a
timothy3001 <t.knufmann@gmail.com>
2015-11-18 14:31:09 +0100

    Main-Klasse angelegt, Benchmarkportal-Anmeldedaten hinterlegt

---
+++ Aufgabe 7/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	private static boolean initializeConnection() {
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		if (!initializeConnection()) { 
+			System.out.println("Could not establish connection to database!");
+			System.exit(-1);
+		}	
+		
+	}
+
+}
