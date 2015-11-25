19eaf78c8dcb4583c3b69896f69efdffb79a3e42
rek <rek@ghorm>
2015-11-25 14:34:17 +0100

    Auf PreparedStatment umgebaut

--- Aufgabe 7/src/com/whs/dbi21/benchmark/Statements.java
+++ Aufgabe 7/src/com/whs/dbi21/benchmark/Statements.java
@@ -1,6 +1,7 @@
 package com.whs.dbi21.benchmark;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
@@ -8,22 +9,45 @@ import java.util.Random;
 public class Statements {
 	static void createdb(int pn, Connection pconnection){
 		
-		Statement stmt=null;
+		PreparedStatement stmt=null;
 		String name=createString(20);
 		String addresslong=createString(72);
 		String addressshort=createString(68);
 		
 		try {
-			stmt = pconnection.createStatement();
+			stmt = pconnection.prepareStatement("INSERT INTO branches (branchid, balance, branchname, address) " + "VALUES (?,0,'"+name+"','"+addresslong+"')");
 			
-			for (int i=0;i<pn;i++)
-				stmt.executeUpdate("INSERT INTO branches (branchid, balance, branchname, address) " + "VALUES ("+(i+1)+",0,'"+name+"','"+addresslong+"')");
+			for (int i=0;i<pn;i++){
+				stmt.setInt(1, i+1);
+				stmt.executeUpdate();
+			}
 			
-			for (int i=0;i<pn*100000;i++)
-				stmt.executeUpdate("INSERT INTO accounts (accid, balance, branchid, name, address)" + "VALUES ("+(i+1)+",0,"+((int)(Math.random()*pn)+1)+",'"+name+"','"+addressshort+"')");
-		
-			for (int i=0;i<pn*10;i++)
-				stmt.executeUpdate("INSERT INTO tellers (tellerid, balance, branchid, tellername, address)" + "VALUES ("+(i+1)+",0,"+((int)(Math.random()*pn)+1)+",'"+name+"','"+addressshort+"')");
+			stmt.close();
+			//stmt.executeUpdate("INSERT INTO branches (branchid, balance, branchname, address) " + "VALUES ("+(i+1)+",0,'"+name+"','"+addresslong+"')");
+			
+			
+			
+			stmt = pconnection.prepareStatement("INSERT INTO accounts (accid, balance, branchid, name, address)" + "VALUES ("+"?"+",0,"+"?"+",'"+name+"','"+addressshort+"')");
+			
+			for (int i=0;i<pn*100000;i++){
+				stmt.setInt(1, i+1);
+				stmt.setInt(2, ((int)(Math.random()*pn)+1));
+				stmt.executeUpdate();
+			}
+			
+			stmt.close();
+			
+			
+			stmt = pconnection.prepareStatement("INSERT INTO tellers (tellerid, balance, branchid, tellername, address)" + "VALUES ("+"?"+",0,"+"?"+",'"+name+"','"+addressshort+"')");
+			
+			for (int i=0;i<pn*10;i++){
+				stmt.setInt(1, i+1);
+				stmt.setInt(2, ((int)(Math.random()*pn)+1));
+				stmt.executeUpdate();
+			}
+			
+			stmt.close();
+				//stmt.executeUpdate("INSERT INTO tellers (tellerid, balance, branchid, tellername, address)" + "VALUES ("+(i+1)+",0,"+((int)(Math.random()*pn)+1)+",'"+name+"','"+addressshort+"')");
 
 			
 			
