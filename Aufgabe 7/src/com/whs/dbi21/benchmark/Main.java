ebcd3f2741f48ee188da1e00daa4fe887a527221
timothy3001 <t.knufmann@gmail.com>
2015-11-26 21:59:29 +0100

    Kommentare hinzugefügt

--- Aufgabe 7/src/com/whs/dbi21/benchmark/Main.java
+++ Aufgabe 7/src/com/whs/dbi21/benchmark/Main.java
@@ -10,10 +10,30 @@ import java.util.Scanner;
 
 import com.whs.dbi21.benchmark.DbConnectionInfo;
 
+/**
+ * Benchmark-Test für MySQL
+ * 
+ * In diesem Benchmark-Test werden abhängig von n viele INSERT-Statements zur Datenbank geschickt. Beim Aufruf
+ * der main-Methode hat der User die Möglichkeit einen Wert für n einzutragen. Üblicherweise werden Werte von 1-50
+ * für n verwendet, um die Datenbank zu testen. 
+ * 
+ * Sobald der Test durchgelaufen ist, wird die Dauer in Sekunden zurückgegeben. 
+ * 
+ * Des Weiteren werden die Tabellen zur Vorbereitung des Tests beim Aufruf der main-Methode geleert. 
+ * 
+ * @author Johannes Nowack, André Schlüß, Timo Knufmann
+ *
+ */
 public class Main {
 
 	private static Connection dbCon;
 	
+	/**
+	 * Stellt die Verbindung zu einem DBMS her. Wenn eine Verbindung erfolgreich hergestellt werden konnte,
+	 * wird true zurückgegeben. 
+	 * 
+	 * @return Gibt false zurück, falls es einen Fehler beim Herstellen der Verbindung zu einem DBMS gab
+	 */
 	private static boolean initializeConnection() {
 		try {
 			dbCon = DriverManager.getConnection(DbConnectionInfo.JDBCSTRING, DbConnectionInfo.DBUSER, DbConnectionInfo.DBPASSWORD);
@@ -24,33 +44,49 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * Start des Programms. Zu erst wird versucht eine Verbindung zu einem DBMS herzustellen. Anschließend
+	 * werden die Tabellen geleert. Daraufhin wird der eigentliche Benchmark-Test ausgeführt und am Ende
+	 * wird die benötigte Dauer in Sekunden zurückgegeben.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// Herstellen der Verbindung zur Datenbank		
 		if (!initializeConnection()) { 
 			System.out.println("Could not establish connection to database!");
 			System.exit(-1);
 		}			
 		System.out.println("Connection to database established!");		
 		
+		// Aufräumen der Tabellen
 		cleanDatabase();				
 		System.out.println("Database cleaned!");
 		
+		// Einlesen von n. Falls Wert nicht gültig, wird der default 10 verwendet
 		System.out.print("Please enter a number for n: ");
 		int n = readNumberInInt();
 		if (n < 1) {
 			System.out.println("No valid number for n was entered, using default: 10");
 			n = 10;
-		}
+		}		
 		
-		
+		// Starten des Benchmark-Tests und festhalten der Zeitpunkte
 		System.out.println("Start benchmark test!");
 		Date testStart = new Date();
 		Statements.createdb(n, dbCon);	
 		Date testFinish = new Date();
 		
+		// Ausgabe der benötigten Zeit in Sekunden
 		long timeUsed = testFinish.getTime() - testStart.getTime();
 		System.out.println("Time used in seconds = " + (double)timeUsed / 1000);		
 	}
 
+	/**
+	 * Bereinigt die Benchmark-Test Datenbank.
+	 * 
+	 * @return Gibt true zurück, wenn das Bereinigen erfolgreich ausgeführt werden konnte.
+	 */
 	private static boolean cleanDatabase() {
 		Statement st;		
 		try {
@@ -74,6 +110,12 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * Liest eine Integervariablen aus dem STDIN ein und gibt diese zurück. Falls es sich um keine gültige Zahl handelt,
+	 * wird -1 zurückgegeben
+	 * 
+	 * @return Eingelesene Integerzahl
+	 */
     private static int readNumberInInt() {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.next();
--- Aufgabe 7/src/com/whs/dbi21/benchmark/Statements.java
+++ Aufgabe 7/src/com/whs/dbi21/benchmark/Statements.java
@@ -6,36 +6,53 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
 
+/**
+ * In dieser Klasse wird der eigentliche Benchmark-Test durchgeführt. Dafür wird eine bereits bestehende Verbindung
+ * zu einer Datenbank benötigt.
+ * 
+ * @author André Schlüß, Johannes Nowack, Timo Knufmann
+ *
+ */
 public class Statements {
+	
+	/**
+	 * Führt den eigentlichen Benchmark-Test durch. Es werden n Datensätze in der "branches" Tabelle angelegt.
+	 * Des Weiteren werden n * 100000 Datensätze in der "accounts" Tabelle angelegt und n * 10 Datensätze in der
+	 * "tellers" Datenbank.
+	 * 
+	 * @param pn Faktor für die Anzahl der anzulegenden Daten
+	 * @param pconnection Datenbankverbindung zur Datenbank, auf der der Test durchgeführt werden soll
+	 */
 	static void createdb(int pn, Connection pconnection){
 		
 		PreparedStatement stmt=null;
-		String name=createString(20);
-		String addresslong=createString(72);
-		String addressshort=createString(68);
+		
+		// Erzeugung zufälliger Strings, die im Benchmark als "Dummies" verwendet werden
+		String name = createString(20);
+		String addresslong = createString(72);
+		String addressshort = createString(68);
 		
 		try {
-			
+			// Deaktivierung AutoCommit (Optimierung)
 			pconnection.setAutoCommit(false);
+			
+			// Verwendung von Prepared Statements (ebenfalls Optimierung)
 			stmt = pconnection.prepareStatement("INSERT INTO branches (branchid, balance, branchname, address) " + "VALUES (?,?,?,?)");
 			
 			for (int i=0;i<pn;i++){
+				// Einfügen von Datensätzen in die Tabelle "branches" mithilfe der Prepared Statements
 				stmt.setInt(1, i+1);
 				stmt.setInt(2, 0);
 				stmt.setString(3, name);
 				stmt.setString(4, addresslong);
 				stmt.addBatch();
-				//stmt.executeUpdate();
 			}
 			
-			stmt.executeBatch();
-		//	pconnection.commit();
+			// Ausführung des ersten Stapels, für die "branches" Tabelle
+			stmt.executeBatch();			
+			stmt.close();		
 			
-			stmt.close();
-			//stmt.executeUpdate("INSERT INTO branches (branchid, balance, branchname, address) " + "VALUES ("+(i+1)+",0,'"+name+"','"+addresslong+"')");
-			
-			
-			
+			// INSERT-Statements für die Tabelle "accounts" vorbereiten und ausführen
 			stmt = pconnection.prepareStatement("INSERT INTO accounts (accid, balance, branchid, name, address)" + "VALUES (?,?,?,?,?)");
 			
 			for (int i=0;i<pn*100000;i++){
@@ -44,17 +61,14 @@ public class Statements {
 				stmt.setInt(3, ((int)(Math.random()*pn)+1));
 				stmt.setString(4, name);
 				stmt.setString(5, addressshort);
-				//stmt.executeUpdate();
 				stmt.addBatch();
 			}
 			
 			stmt.executeBatch();
 			
-			//pconnection.commit();
-			
 			stmt.close();
 			
-			
+			// INSERT-Statements für die "tellers" Tabelle vorbereiten und ausführen
 			stmt = pconnection.prepareStatement("INSERT INTO tellers (tellerid, balance, branchid, tellername, address)" + "VALUES (?,?,?,?,?)");
 			
 			for (int i=0;i<pn*10;i++){
@@ -69,18 +83,21 @@ public class Statements {
 			
 			stmt.executeBatch();
 			
+			// Commit in der Datenbank aufrufen
 			pconnection.commit();
 			
 			stmt.close();
-				//stmt.executeUpdate("INSERT INTO tellers (tellerid, balance, branchid, tellername, address)" + "VALUES ("+(i+1)+",0,"+((int)(Math.random()*pn)+1)+",'"+name+"','"+addressshort+"')");
-
-			
-			
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Erzeugt einen zufälligen String der Länge azchar
+	 * 
+	 * @param azchar Länge des zufälligen Strings
+	 * @return Zufälliger String
+	 */
 	static String createString(int azchar){
 		String rString="";
 		Random rnd=new Random();
