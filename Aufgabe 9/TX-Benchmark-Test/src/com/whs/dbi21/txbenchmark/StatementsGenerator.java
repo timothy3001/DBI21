1c3987f6e1d48bec76b1c376e377bc602f8df0e0
timothy3001 <t.knufmann@gmail.com>
2015-12-10 00:03:37 +0100

    Funktionsrümpfe für Statements angelegt

--- Aufgabe 9/TX-Benchmark-Test/src/com/whs/dbi21/txbenchmark/StatementsGenerator.java
+++ Aufgabe 9/TX-Benchmark-Test/src/com/whs/dbi21/txbenchmark/StatementsGenerator.java
@@ -1,5 +1,30 @@
 package com.whs.dbi21.txbenchmark;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class StatementsGenerator {
 
+	public static int executeBalanceTx(int accId, Connection dbCon) throws SQLException {
+		Statement st = dbCon.createStatement();
+		
+		ResultSet rs = st.executeQuery("SELECT balance FROM accounts WHERE accid = " + Integer.toString(accId) + ";");
+		rs.next();
+		
+		int result = rs.getInt(0);
+		rs.close();
+		st.close();
+		
+		return result;
+	}
+	
+	public static void executeInpaymentTx(int accId, int tellerId, int branchId, int delta, Connection dbCon) {
+		
+	}
+	
+	public static int executeAnalyseTx(int delta, Connection dbCon) {
+		return 0;
+	}
 }
