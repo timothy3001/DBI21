e2f5d7c333dc590e806d592c8811afcf47ce6fac
timothy3001 <t.knufmann@gmail.com>
2015-12-09 14:30:06 +0100

    Verbindung zur DB wird hergestellt Package korrigiert

---
+++ Aufgabe 9/TX-Benchmark-Test/src/com/whs/dbi21/txbenchmark/Main.java
@@ -0,0 +1,28 @@
+package com.whs.dbi21.txbenchmark;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+	private static Connection dbCon;
+	
+	private static void initializeConnection() throws SQLException {
+		dbCon = DriverManager.getConnection(DbConnectionInfo.JDBCSTRING, DbConnectionInfo.DBUSER, DbConnectionInfo.DBPASSWORD);
+	}
+	
+	public static void main(String[] args) {
+		// Herstellen der Verbindung zur Datenbank		
+		try {
+			initializeConnection();
+			System.out.println("Connection to database established!");		
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("\nCould not establish connection to database!");			
+			System.exit(-1);
+		}			
+		
+	}
+
+}
