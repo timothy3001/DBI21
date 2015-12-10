0c4eaf96da49e47596e87da2700775cadf847884
timothy3001 <t.knufmann@gmail.com>
2015-12-10 11:38:31 +0100

    Implementierung InpaymentTX

--- Aufgabe 9/TX-Benchmark-Test/src/com/whs/dbi21/txbenchmark/StatementsGenerator.java
+++ Aufgabe 9/TX-Benchmark-Test/src/com/whs/dbi21/txbenchmark/StatementsGenerator.java
@@ -10,7 +10,7 @@ public class StatementsGenerator {
 	public static int executeBalanceTx(int accId, Connection dbCon) throws SQLException {
 		Statement st = dbCon.createStatement();
 		
-		ResultSet rs = st.executeQuery("SELECT balance FROM accounts WHERE accid = " + Integer.toString(accId) + ";");
+		ResultSet rs = st.executeQuery("SELECT balance FROM accounts WHERE accid = " + accId + ";");
 		rs.next();
 		
 		int result = rs.getInt(0);
@@ -20,11 +20,22 @@ public class StatementsGenerator {
 		return result;
 	}
 	
-	public static void executeInpaymentTx(int accId, int tellerId, int branchId, int delta, Connection dbCon) {
+	public static int executeInpaymentTx(int accId, int tellerId, int branchId, int delta, Connection dbCon) throws SQLException {
+		Statement st = dbCon.createStatement();
 		
+		st.executeUpdate("UPDATE branches SET balance = balance + " + delta + " WHERE branchid = " + branchId + ";");
+		st.executeUpdate("UPDATE tellers SET balance = balance + " + delta + " WHERE tellerid = " + tellerId + ";");
+		st.executeUpdate("UPDATE accounts SET balance = balance + " + delta + " WHERE accid = " + accId + ";");
+		
+		int currentBalance = executeBalanceTx(accId, dbCon);
+		st.executeUpdate("INSERT INTO history (accid, tellerid, delta, branchid, accbalance, cmmnt) VALUES(" + accId + ", " + tellerId + ", " + delta + ", " + branchId + ", " + currentBalance + ", 'INPAYMENT');");
+		
+		st.close();
+		
+		return currentBalance;
 	}
 	
-	public static int executeAnalyseTx(int delta, Connection dbCon) {
+	public static int executeAnalyseTx(int delta, Connection dbCon) throws SQLException {
 		return 0;
 	}
 }
